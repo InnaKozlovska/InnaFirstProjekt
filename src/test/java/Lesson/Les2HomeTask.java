afcdd2b4ee165500b6eb8e9f9873b01f9986e9a2
ITInna <i.kozlovskapost@gmail.com>
2025-09-23 22:09:27 +0300

    урок 2 домашка

---
+++ src/test/java/Lesson/Les2HomeTask.java
@@ -0,0 +1,48 @@
+package Lesson;
+
+public class Les2HomeTask {
+    public static void main(String[] args) {
+
+        /*
+1) Создать по одной переменной каждого типа данных, который ты знаешь.
+2) Присвоить каждой переменной значение в диапазоне типа.
+3) Вывести значение каждой переменной в консоль, каждое значение выводить с новой строки.
+*/
+/*
+1) Создать три переменных (number1, number2, number3) типа int, присвоить им значение 10, 18, 21 соответственно.
+2) Не используя чисел, сделать так, чтобы значение у переменной number1 изменилось на 18
+2) Не используя чисел, сделать так, чтобы значение у переменной number2 изменилось на 21
+2) Не используя чисел, сделать так, чтобы значение у переменной number3 изменилось на 10
+3) Вывести значение каждой переменной в консоль, каждое значение выводить с новой строки.
+*/
+    //1
+        int number = 20;
+        System.out.println(number);
+        short s = 5000;
+        System.out.println(s);
+        char c = '+';
+        System.out.println(c);
+        boolean b1 = true;
+        System.out.println(b1);
+
+
+        //2
+        int number1 = 10;
+        int number2 = 18;
+        int number3 = 21;
+
+        number1 =18;
+        number2 =21;
+        number3 =10;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+
+    }
+
+
+}
+
+
+

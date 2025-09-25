80edd2f7b4f27afbaa6087a011d6c025b9fa0741
ITInna <i.kozlovskapost@gmail.com>
2025-09-25 22:53:33 +0300

    урок 5

---
+++ src/test/java/Lesson/Les5Main.java
@@ -0,0 +1,12 @@
+package Lesson;
+
+public class Les5Main {
+    public static void main(String[] args) {
+        Les5ObjektUser user = new Les5ObjektUser();
+        user.age = 18;
+        user.name = "Alex";
+        user.passport = "pas";
+        System.out.println(user.name);
+
+    }
+}
